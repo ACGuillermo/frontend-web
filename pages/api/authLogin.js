05a5cb4317f8cb1869b9fb29b2d17cb79d19d646
GuillermoAguilar <39215353+ACGuillermo@users.noreply.github.com>
2020-08-25 15:06:51 +0200

    fix

--- pages/api/authLogin.js
+++ pages/api/authLogin.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 export default (req, res) => {
   console.log('Lammada auth api in nextjs')
-    axios.post('auth:3000/api/auth/login',{
+    axios.post('http://auth:3000/api/auth/login',{
         user: "test",
         password: "test2"
       })
