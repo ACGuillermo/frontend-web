0225730c18915cd2bd613e5f8fff18524b04492c
GuillermoAguilar <39215353+ACGuillermo@users.noreply.github.com>
2020-08-25 13:13:29 +0200

    Testing auth service

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Link from 'next/link'
+const axios = require('axios');
 
 function About() {
+  useEffect(()=>{
+    axios.post('http://auth:3000/api/auth/login',{
+      user: "test",
+      password: "test2"
+    })
+    .then(res => console.log(res.data))
+    .catch(error => console.log(error.response))
+  },[])
   return (
     <>
       <Link href="/">
