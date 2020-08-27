f744e6f3e05422bcd9b6b3d6bd4f0fa241f53903
GuillermoAguilar <39215353+ACGuillermo@users.noreply.github.com>
2020-08-27 19:21:35 +0200

    no preconnect

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="preconnect" rel="stylesheet"/>
+          <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet"/>
         </Head>
         <body>
           <Main />
