471ce2baa7bce2551f82588e928a2202ad9eb7d5
GuillermoAguilar <39215353+ACGuillermo@users.noreply.github.com>
2020-08-27 21:48:56 +0200

    Test img

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,9 @@ import Header from '../components/header/Header'
 
 export default function Home() {
   return (
-    <Header />
+    <>
+      <Header />
+      <img className="h-64 w-64"  src="https://i.redd.it/w3kr4m2fi3111.png" alt=""/>
+    </>
   )
 }
