e3642133fba3557b0d828309cadb67d762ec69e7
GuillermoAguilar <39215353+ACGuillermo@users.noreply.github.com>
2020-08-27 21:16:58 +0200

    Fixed sideDrawer showing on first render

--- components/header/SideDrawer.jsx
+++ components/header/SideDrawer.jsx
@@ -5,11 +5,10 @@ const SideDrawer = ({click, show}) =>{
         drawerClasses = 'md:hidden side-drawer open'
         
     }
-    console.log(drawerClasses)
 
     return (
         <nav className={drawerClasses}>
-            <button onClick={click} className="fixed top-0 right-0 mt-4 mr-4 fill-current text-teal-400">
+            <button onClick={click} className="relative top-0 right-0 mt-4 mr-4 fill-current text-teal-400 focus:outline-none">
                 <svg className="h-10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                     <path d="M368 214L161 6a20 20 0 00-29 28l208 208c7 8 7 20 0 28L132 478a20 20 0 1029 28l207-208c23-23 23-61 0-84z"/>
                 </svg>
